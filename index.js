3b071d97ad84986ad227058ee2e13f4d03175adf
HARRY BHATT <bob@brahmingate.co.uk>
2019-01-30 17:33:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+
+function theBeatlesPlay (musicians, instruments) {
+var artists = [];
+
+for (var counter = 0; counter < musicians.length; counter++) {
+  var artistSkill = musicans[counter] + ' plays ' + instruments[counter];
+  artists[counter] = artistSkill;
+}
+ return artists;
+}
+
