aadda7c31c8b6719c1fefa8803fd7b7c2beceb93
HARRY BHATT <bob@brahmingate.co.uk>
2019-01-30 17:35:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (musicians, instruments) {
 var artists = [];
 
 for (var counter = 0; counter < musicians.length; counter++) {
-  var artistSkill = musicans[counter] + ' plays ' + instruments[counter];
+  var artistSkill = musicians[counter] + ' plays ' + instruments[counter];
   artists[counter] = artistSkill;
 }
  return artists;
